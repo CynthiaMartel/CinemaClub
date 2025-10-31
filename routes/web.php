091947317e933dc14c057e4bbbd38f0b7ad84baf
Cynthia <cynthiamartelmartin@gmail.com>
2025-10-31 17:08:32 +0000

    refactor: improve DB seeding route and pagination -- refactor: mejorar ruta de llenado de BD y paginación

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,30 @@ Route::get('/wikidata/test_title_wikidata/{title}', [TestsDBApisController::clas
 
 //----------------------------------------------------------------------------------------------------------------
 
-//Routa para importar desde APIs wikidata y tmdb --> poblar y guardar en BD (si en la función se cambia limit por un núnero reducido, puede servir de prueba rápida para ver si se puebla la BD correctamente)
-Route::post('/films/import/{yearStart}/{yearEnd}/{pages?}', [FilmDataController::class, 'importFromTMDb'])->name('films.import');
+//Routa para importar desde APIs wikidata y tmdb --> poblar y guardar en BD (si en la función se cambia la variable limit por un núnero reducido, puede servir de prueba rápida para ver si se puebla la BD correctamente)
+Route::post('/films/import/{yearStart}/{yearEnd}/{startPage?}/{endPage?}', [FilmDataController::class, 'importFromTMDB'])->name('films.import');
 
+// Ruta para manejar el Job por si usamos Postman o desde la Web
+Route::post('/films/import/{start}/{end}/{from}/{to}', function($start,$end,$from,$to){'\App\Jobs\ImportFilmsJob'::dispatch((int)$start,(int)$end,(int)$from,(int)$to);
+    return response()->json([
+        'message'=>"Job encolado {$start}-{$end}, páginas {$from}-{$to}"
+    ]);
+});
 
 Route::post('/import/tmdb/async', [FilmDataController::class, 'importFromTMDBAsync']); // Para invocar un botón (ej en fronted y así importar las películas)
 
 
+// RUTAS AUTH: rutas de autentificación con login, logout y comprobación de sesión. Respuesta Json y tokens Sanctum
+
+// LOGIN: inicia sesión y devuelve token
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// LOGOUT: cierra sesión (requiere token Sanctum)
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum')
+    ->name('logout');
+
+// CHECK SESSION: devuelve usuario autenticado si el token es válido
+Route::get('/check-session', [AuthController::class, 'checkSession'])
+    ->middleware('auth:sanctum')
+    ->name('checkSession');
