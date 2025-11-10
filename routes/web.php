168d8c8fc7bb0d9a4b149b045c3312e2da62312f
Cynthia <cynthiamartelmartin@gmail.com>
2025-11-10 22:25:20 +0000

    feat(routes): add API routes for user entries, lists, comments, and related actions -- feat(routes): añadir rutas API para entradas de usuario, listas, comentarios y acciones relacionadas

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,13 @@ use App\Http\Controllers\FilmDataController;
 use App\Http\Controllers\TestsDBApisController;
 
 use App\Http\Controllers\PostController;
+
 use App\Http\Controllers\UserProfileController;
-use App\Http\Controllers\UserFilmActionController;
+
+use App\Http\Controllers\UserEntryController;
+use App\Http\Controllers\UserCommentController;
+use App\Http\Controllers\UserSavedListController;
+use App\Http\Controllers\UserEntryFilmController;
 
 
 use Illuminate\Support\Facades\Route;
@@ -191,4 +196,99 @@ Route::middleware('auth:sanctum')->prefix('api')->group(function () {
         ->name('api.user.films.stats');
 });
 
+/*
+|--------------------------------------------------------------------------
+|     -- RUTAS USER ENTRIES --
+|--------------------------------------------------------------------------
+*/
+// Rutas para entradas de listas, debates, reseñas
+
+//UserEntryController: Para el control de entradas que crean los usuarios (listas, debates y reseñas)
+Route::middleware('auth:sanctum')->prefix('api')->group(function () {
+
+    //  MOSTRAR feed general de entradas (reviews, debates, listas)
+    //  Permite filtrar por tipo de entrada(reviews, listas, debates), usuario o película asociada. 
+    // Ejemplo: /api/user_entries/feed?type=user_review&user_name=Cynthia
+    Route::get('/user_entries/feed', [UserEntryController::class, 'showEntries'])
+        ->name('api.user_entries.feed');
+
+    //  MOSTRAR una entrada concreta por ID de entrada
+    //  Permite ver el detalle completo de una lista, debate o reseña (según visibilidad)
+    Route::get('/user_entries/{id}', [UserEntryController::class, 'show'])
+        ->name('api.user_entries.show');
+
+    //  CREAR nueva lista, debate o reseña
+    //  Solo usuarios autenticados pueden crear entradas
+    Route::post('/user_entries/create', [UserEntryController::class, 'store'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.user_entries.create');
+
+    //  ELIMINAR una entrada (solo para user o admin)
+    Route::delete('/user_entries/{id}', [UserEntryController::class, 'destroy'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.user_entries.destroy');
+
+    //  ME GUSTA : Dar o quitar “like” a una entrada
+    //  Se actualiza automáticamente el contador likes_count
+    Route::post('/user_entries/{entryId}/like', [UserEntryController::class, 'toggleLike'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.user_entries.toggleLike');
+
+    //  GUARDAR o quitar una lista (en user_saved_lists) SOLO TIPO USER_LISTS
+    //  Permite a los usuarios guardar o eliminar listas y después lo mostamos en su perfil
+    Route::post('/user_entries_lists/{entryId}/save', [UserEntryController::class, 'toggleSaveList'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.user_entries.toggleSaveList');
+
+    //  MOSTRAR las listas creadas por un usuario (para uso en su perfil personal)
+    //  Muestra todas las listas creadas por un usuario concreto en su perfil personal
+    Route::get('/user_profiles/{userId}/lists', [UserEntryController::class, 'showUserLists'])
+        ->name('api.user_profiles.lists');
+
+
+});
+
+// ----USER LISTS---
+// UserSavedListController: Listas que crean los usuarios (ej: "Top 5 películas de terror 2025") 
+
+Route::middleware('auth:sanctum')->prefix('api')->group(function () {
+
+    // GUARDAR O ELIMINAR listas (solo type = user_list)  
+    Route::post('/user_entries/{entryId}/save', [UserSavedListController::class, 'toggle'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.user_saved_lists.toggle');
+
+     // ASOCIAR entradas a películas
+    Route::post('/user_entry_films/create', [UserEntryFilmController::class, 'store'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.user_entry_films.store');
+
+    // MOSTRAR películas asociadas a una entrada con id de entrada
+    Route::get('/user_entry_films/{entryId}/showByFilm', [UserEntryFilmController::class, 'showByFilm'])
+        ->name('api.user_entry_films.show');
+
+    // ELIMINAR la relación película-entrada con id de la asociación UserEntryFilm
+    Route::delete('/user_entry_films/{id}/delete', [UserEntryFilmController::class, 'destroy'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.user_entry_films.destroy');
+
+
+    // --USER COMMENTS --
+    // UserCommentController: comentarios con polimorfismos para usar en films (comentarios de películas) o en entradas (comentarios en entradas de usuarios)
+     
+    // OBTENER comentarios (de un film o una entry) por IdFilm o o id de la entrada
+    Route::get('/comments/{type}/{id}', [UserCommentController::class, 'index'])
+        ->name('api.comments.index');
+
+    // CREAR comentario (en film o entry) por IdFilm o o id de la entrada
+    Route::post('/comments/{type}/{id}/create', [UserCommentController::class, 'store'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.comments.store');
+
+    // ELIMINAR comentario por ID
+    Route::delete('/comments/{commentId}/delete', [UserCommentController::class, 'destroy'])
+        ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class)
+        ->name('api.comments.destroy');
+
+});
 
